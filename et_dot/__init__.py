c5646556f6f709a7aa710881e0cfcf24ca0c4b37
Engelbert Tijskens <engelbert.tijskens@uantwerpen.be>
2019-12-13 13:28:57 +0100

    "first commit"

---
+++ et_dot/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Package et_dot
+=======================================
+
+Top-level package for et_dot.
+"""
+
+__version__ = "0.0.0"
+
+
+def hello(who='world'):
+    """'Hello world' method.
+
+    :param str who: whom to say hello to
+    :returns: a string
+    """
+    result = "Hello " + who
+    return result
+
+# Your code here...
